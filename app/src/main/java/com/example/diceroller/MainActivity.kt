2af9553cf6258a8864d1f05481127abf9d61a1cb
Intan Yoshana Oewen <34388370+intan318@users.noreply.github.com>
2019-08-20 16:39:37 +0700

    change diceImage local variable into a field
    to reduce the use of findViewById whenever the rollDIce() function is called

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.diceroller
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,6 +11,13 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    //ngasih initial value which is null tapi nanti kalo = null hrs diinit lg di dlm oncreate trs pake method nullcheck dulu
+    //jadi null diapus trs diganti jadi pake lateinit promises the compiler that the variable will be initialized before calling any operations on it
+    //promising that we will not leave it as null
+//    var diceImage: ImageView? = null
+    lateinit var diceImage: ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
-
+        diceImage = findViewById(R.id.imgDice)
     }
 
     private fun rollDice() {
@@ -34,7 +42,8 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val diceImage: ImageView = findViewById(R.id.imgDice)
+        //minimalisasi penggunaan findviewbyid krn ini makan memori, bakal ngelag kalo di app complex dipake trll banyak
+        //val diceImage: ImageView = findViewById(R.id.imgDice)
         diceImage.setImageResource(drawableResource)
     }
 }
