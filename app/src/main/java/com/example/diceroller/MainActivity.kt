63fec7fa0bb19539c1c2206346af301019535ee2
Intan Yoshana Oewen <34388370+intan318@users.noreply.github.com>
2019-08-20 16:24:43 +0700

    changed the randomNumber Text to Image
    after the roll button is clicked it will roll the dice image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,25 +3,38 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.rollButton)
+        val rollButton: Button = this.findViewById(R.id.rollButton)
         rollButton.setOnClickListener {
             rollDice()
-
         }
 
     }
 
     private fun rollDice() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        val resultText: TextView = findViewById(R.id.txtResult)
-        resultText.text = "Dice rolled!"
+        //Random().nextInt(6) => ini bakal ngasih nomor random antara 0 - 5, supaya randomnya dari 1 - 6 jadi (6)+1
+        val randomInt = Random.nextInt(6) +1
+
+//        bikin variabel drawableResource yg isinya when expression ketika angka yg keluar 1 dia ngeluarin gambar dice_1 dst
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val diceImage: ImageView = findViewById(R.id.imgDice)
+        diceImage.setImageResource(drawableResource)
     }
 }
